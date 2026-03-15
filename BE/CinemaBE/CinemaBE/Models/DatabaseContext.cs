d3cf8bb81764ee1bacd5e7058180e1c82b7ae836
trungnguyen <trungnguyen020103@gmail.com>
2026-03-15 12:48:13 +0700

    fix ketnoidb

--- BE/CinemaBE/CinemaBE/Models/DatabaseContext.cs
+++ BE/CinemaBE/CinemaBE/Models/DatabaseContext.cs
@@ -51,9 +51,9 @@ public partial class DatabaseContext : DbContext
 
     public virtual DbSet<TblTimeSlot> TblTimeSlots { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=cinema;Integrated Security=True;Encrypt=False;TrustServerCertificate=True");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=MSI\\SQLEXPRESS;Database=cinema;Integrated Security=True;Encrypt=False;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
