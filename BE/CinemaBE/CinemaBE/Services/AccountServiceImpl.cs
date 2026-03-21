aa6425e5b22bf59a329c621e88707060ecef6ac7
trungnguyen <trungnguyen020103@gmail.com>
2026-03-21 08:02:02 +0700

    feature : register account for users

--- BE/CinemaBE/CinemaBE/Services/AccountServiceImpl.cs
+++ BE/CinemaBE/CinemaBE/Services/AccountServiceImpl.cs
@@ -1,4 +1,6 @@
-﻿using CinemaBE.Models;
+﻿using CinemaBE.Dtos;
+using CinemaBE.Helpers;
+using CinemaBE.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CinemaBE.Services
@@ -15,6 +17,51 @@ namespace CinemaBE.Services
         {
             var accounts = _db.SysAccounts.ToListAsync();
             return await accounts;
+        }
+
+        public async Task<SysAccountResponseDto> RegisterAccountAsync(SysAccountRegisterDto sysAccountRegisterDto)
+        {
+            if (sysAccountRegisterDto.Password != sysAccountRegisterDto.ConfirmPassword)
+                throw new Exception("Mật khẩu xác nhận không khớp");
+
+            string username = sysAccountRegisterDto.Username.Trim();
+            string email = sysAccountRegisterDto.Email.Trim().ToLower();
+            string phone = sysAccountRegisterDto.PhoneNumber!.Trim();
+
+            if (await _db.SysAccounts.AnyAsync(x => x.Username == username))
+                throw new Exception("Username đã tồn tại");
+
+            if (await _db.SysAccounts.AnyAsync(x => x.Email == email))
+                throw new Exception("Email đã tồn tại");
+
+            if (await _db.SysAccounts.AnyAsync(x => x.PhoneNumber == phone))
+                throw new Exception("Số điện thoại đã tồn tại");
+
+            var account = new SysAccount
+            {
+                Username = username,
+                Email = email,
+                PhoneNumber = phone,
+                Password = AccountHelper.HashPassword(sysAccountRegisterDto.Password),
+                FullName = sysAccountRegisterDto.Fullname,
+                Dob = sysAccountRegisterDto.Dob,
+                Role = "User",
+                Status = true
+            };
+
+            await _db.SysAccounts.AddAsync(account);
+            await _db.SaveChangesAsync();
+
+            return new SysAccountResponseDto
+            {
+                Id = account.Id,
+                Username = account.Username,
+                Dob = account.Dob,
+                Email = account.Email,
+                PhoneNumber = account.PhoneNumber,
+                Role = account.Role,
+                Status = account.Status
+            };
         }
     }
 }
